df47a47c1914c3695c2ffc24ae89ad632f3f7d72
karthiksivt <85487378+karthiksivt@users.noreply.github.com>
2021-12-06 13:59:26 +0530

    javascript-newsapi
    javascript-newsapi

---
+++ app.js
@@ -0,0 +1,23 @@
+var button = document.querySelector('.button')
+var inputValue = document.querySelector('.inputValue')
+
+var cityname = document.querySelector('.cityname');
+var desc = document.querySelector('.desc');
+var temp = document.querySelector('.temp');
+
+
+button.addEventListener('click',function(){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=f050924bac0914f9f3fca2b30f9913ec')
+    .then(response => response.json())
+    .then(data => {
+        var nameValue = data['name'];
+        var tempValue = data['main']['temp'];
+        var descValue = data['weather'][0]['description'];
+
+       cityname.innerHTML = nameValue;
+       temp.innerHTML = tempValue;
+       desc.innerHTML = descValue
+    })
+
+.catch(err => alert("Wrong city name"))
+})
